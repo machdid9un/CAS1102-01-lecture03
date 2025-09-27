4bf061ae85e0afcee198f7e0dd7e32e3b9434c79
김건 <lyugayeon@gimgeonui-MacBook-Air.local>
2025-09-27 15:48:23 +0900

    modified

--- practice.py
+++ practice.py
@@ -1,26 +1,60 @@
 from typing import List
-
+import math
 
 class Shape:
     # TODO: Define the method "measure".
     """
     - This is an abstract method, so it should just raise NotImplementedError.
     """
+    def measure(self):
+        raise NotImplementedError
+    
+    
 
 
 # TODO: Create a Circle class that inherits from Shape.
 # - Initialize it with a radius (float).
 # - Implement the measure method to return the area of the circle.
-
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius= float(radius)
+    def measure(self):
+        return self.radius**2*math.pi
+        
 
 # TODO: Create a Rectangle class that inherits from Shape.
 # - Initialize it with width and height (floats).
 # - Implement the measure method to return the area of the rectangle.
-
+class Rectangle(Shape):
+    def __init__(self,width,height):
+        self.width= float(width)
+        self.height= float(height)
+    def measure(self):
+        return self.width*self.height
+        
 
 # TODO: Create a Triangle class that inherits from Shape.
 # - Initialize it with three vertices, each a tuple of (x, y).
 # - Implement the measure method to return the area of the triangle using Heron's formula.
+class Triangle(Shape):
+    def __init__(self, p1, p2, p3):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+
+    def measure(self):
+        
+        a = math.dist(self.p1, self.p2)
+        b = math.dist(self.p2, self.p3)
+        c = math.dist(self.p3, self.p1)
+
+       
+        s = (a + b + c) / 2
+
+       
+        A = math.sqrt(s * (s - a) * (s - b) * (s - c))
+        return A
+
 
 
 if __name__ == "__main__":
